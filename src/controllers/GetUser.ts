6d9ebd8540f54ac8f53fc56db22e6ec21378db1c
leonel <leoonelvj@gmail.com>
2024-01-28 04:46:38 -0300

    modificando y agregando archivos para mostrar usuarios

--- src/controllers/GetUser.ts
+++ src/controllers/GetUser.ts
@@ -1,16 +1,20 @@
-import { Request, Response } from "express";
-import { User } from "../database/entities/User";
+// En GetUser.ts
+import { Request, Response } from 'express';
+import { User } from '../database/entities/User';
 
 class GetUser {
-    async handle(req: Request, res: Response) {
-        try {
-            const users = await User.find();
-            res.status(200).json(users);
-        } catch (error) {
-            console.error("Error retrieving users:", error);
-            res.status(500).json({ error: "Internal Server Error" });
-        }
+  async handle(req: Request, res: Response) {
+    try {
+        
+      const users = await User.find();  
+      return res.render("users", {
+        users: users
+      });   
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error interno del servidor' });
     }
+  }
 }
 
 export { GetUser };
